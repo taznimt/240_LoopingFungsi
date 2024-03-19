f4c876535d9ad6eb48e47636d25392c1d264b0db
taznimt <taznimanwar06@gmail.com>
2024-03-19 15:56:02 +0700

    createprosedurdanfungsi

--- pertemuanprogram3.cpp
+++ pertemuanprogram3.cpp
@@ -2,39 +2,47 @@
 using namespace std;
 
 string nama;
-int nAdidas, nPuma, bNB,nCompass,nCompass,nNike;
-int hAdidas = 300, hNB = 250, hPuma = 150, hNike = 350;
+int nAdidas, nPuma, nNB, nCompass, nNike;
+int hAdidas = 300, hNB = 250, hPuma = 150, hNike = 350; 
 
-void input()
-{
-    cout << "Masukan jumlah Adidas = ";
-    cin >> nAdidas;
+void input(){
 
-    cout << "Masukan jumlah Puma = ";
-    cin >> nPuma;
+cout << "Masukan jumlah Adidas = ";
+cin >> nAdidas;
 
-    cout << "Masukan jumlah New Balance = ";
-    cin >> nNB;
+cout << "Masukan jumlah Puma = ";
+cin >> nPuma;
 
-    cout << "Masukan jumlah Compass = ";
-    cin >> nCompass;
+cout << "Masukan jumlah New Balance = ";
+cin >> nNB;
 
-    cout << "Masukan jumlah Nike = ";
-    cin >> nNike;
+cout << "Masukan jumlah Compass = ";
+cin >> nCompass;
 
+cout << "Masukan jumlah Nike = ";
+cin >> nNike;
 }
 
-int totalMarga()
-{
-    return (nAdidas * hAdidas) + (nPuma * hPuma) + (nNB * hNB) + (nNike * hNike);
-
+int totalHarga(){
+    return (nAdidas * hAdidas) + (nPuma * hPuma) + (nNB * hNB) + (nNike + hNike);
 }
 
 void Display(){
+
     cout << endl;
-    cout <<"===========" <<endl;
+    cout << "==============" << endl;
     cout << endl;
-    cout << "Jumlah puma = << nPum <<endl;
+    ccreate fout << "Jumlah Puma = " << nPuma << endl;
+    cout << "Jumlah Adidas = " << nAdidas << endl;
     cout << endl;
-    cout << endl;
+    cout << "Total Harga = Rp." << totalHarga() << endl;
+}
+
+
+
+int main()
+{
+   input();
+   Display();
+
 }
